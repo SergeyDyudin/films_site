2e0482bb49161421d2e28c5cbaee839074ab1d74
Sergey Dyudin <sergeydyudin@yandex.ru>
2020-12-28 22:10:28 +0300

    Delete from admin site intermediate many-to-many models.
    Like FilmsType, FilmsGenre e.t.c.

--- main/admin.py
+++ main/admin.py
@@ -4,9 +4,9 @@ from . import models
 # Register your models here.
 admin.site.register(models.Films)
 admin.site.register(models.Actors)
-admin.site.register(models.FilmsCountries)
-admin.site.register(models.FilmsDirectors)
-admin.site.register(models.FilmsActors)
+# admin.site.register(models.FilmsCountries)
+# admin.site.register(models.FilmsDirectors)
+# admin.site.register(models.FilmsActors)
 admin.site.register(models.Types)
 admin.site.register(models.Genres)
 admin.site.register(models.Countries)
