1c7fa782f2e2a219196a55a4108525e961c87690
Sergey Dyudin <sergeydyudin@yandex.ru>
2021-01-02 14:10:05 +0300

    Change title of header in django admin site
    Django administartion => Videoarchive Administration

--- films_site/urls.py
+++ films_site/urls.py
@@ -22,3 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+admin.AdminSite.site_header = 'Videoarchive Administration'
