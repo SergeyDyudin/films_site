c490a613bf03e52589c24a067942d02375024468
Sergey Dyudin <sergeydyudin@yandex.ru>
2021-01-25 17:06:25 +0300

    Starting branch records
    Branch records for application view recorded video

--- films_site/settings.py
+++ films_site/settings.py
@@ -33,13 +33,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'main.apps.MainConfig',
+    'records.apps.RecordsConfig',
 ]
 
 MIDDLEWARE = [
